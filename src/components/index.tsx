16079a24ee13c74fcab17410e6d79d5f402affa0
Emival Santos <emivalvictor@hotmail.com>
2021-12-08 19:51:58 -0300

    Build: add exports

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,3 +2,7 @@ export * from "components/Header/Header";
 export * from "components/Card/Card";
 export * from "components/Home/Home";
 export * from "components/PlatformIcon/PlatformIcon";
+export * from "components/GamePage/GamePage";
+export * from "components/GameInfo/GameInfo";
+export * from "components/ParentPlatforms/ParentPlatforms";
+export * from "components/GameDescription/GameDescription";
