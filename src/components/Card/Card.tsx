32d21e502f94649d4b96c32f3134a91e6e9f7117
Emival Santos <emivalvictor@hotmail.com>
2021-12-08 20:08:32 -0300

    Refactor: moving some responsabilities away

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -6,25 +6,13 @@ import {
   CircularProgressLabel,
 } from "@chakra-ui/react";
 
-import { ParentPlatformsProps } from "interfaces/parentPlatformsProps";
 import { GameProps } from "interfaces/gameProps";
 
-import { PlatformIcon } from "components/PlatformIcon/PlatformIcon";
+import { ParentPlatforms, Metacritic } from "components";
 
 import { Card_Image, Card_Text, Card_Wrapper } from "./Card.styles";
 
 export const Card = (game: GameProps) => {
-  const metacriticColor = (value: number) => {
-    if (value > 0 && value <= 45) {
-      return "red.500";
-    }
-    if (value > 45 && value <= 75) {
-      return "yellow.500";
-    }
-    if (value > 75 && value <= 100) {
-      return "green.500";
-    }
-  };
   return (
     <Flex flexDirection="column" {...Card_Wrapper}>
       <Image
@@ -34,26 +22,9 @@ export const Card = (game: GameProps) => {
         alt={`${game.name}`}
       />
       <Text {...Card_Text}>{game.name}</Text>
-      <Flex justifyContent="space-between">
-        <Flex>
-          {game.parent_platforms.map((item: ParentPlatformsProps) => {
-            return (
-              <PlatformIcon
-                key={item.platform.id}
-                platform={item.platform.name}
-              />
-            );
-          })}
-        </Flex>
-        <CircularProgress
-          value={game.metacritic}
-          color={metacriticColor(game.metacritic)}
-          size="35"
-        >
-          <CircularProgressLabel color="white">
-            {game.metacritic}
-          </CircularProgressLabel>
-        </CircularProgress>
+      <Flex alignItems="center" paddingY="0.5rem">
+        <ParentPlatforms parent_platforms={game.parent_platforms} />
+        <Metacritic value={game.metacritic} />
       </Flex>
     </Flex>
   );
